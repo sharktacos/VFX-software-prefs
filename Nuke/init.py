18884669e53a9cc7a9cf93365ecf13440511dbc5
Derek Flood <23034637+sharktacos@users.noreply.github.com>
2021-12-15 09:35:09 -0800

    Update init.py

--- Nuke/init.py
+++ Nuke/init.py
@@ -1,14 +1,8 @@
 
 nuke.pluginAddPath("./denoice")
 nuke.pluginAddPath('./rvnuke')
-nuke.knobDefault("RotoPaint.toolbox", "brush {{brush ltt 0} {clone ltt 0}}")  
+#nuke.knobDefault("RotoPaint.toolbox", "brush {{brush ltt 0} {clone ltt 0}}")  
 
-#OLD:
-import cryptomatte_utilities
-cryptomatte_utilities.setup_cryptomatte()
-
-nuke.pluginAddPath('./Crypto/Cryptomatte-1.2.0/nuke/')
-nuke.pluginAddPath("./NukeSurvivalToolkit")
 
 import nuke
 nuke.knobDefault("Root.colorManagement", "OCIO")
