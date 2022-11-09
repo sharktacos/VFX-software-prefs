f8d9e5fe3302e2722fd3789bd116ee5849cc3039
Derek Flood <23034637+sharktacos@users.noreply.github.com>
2022-11-09 08:31:51 -0800

    studio tools

--- Nuke/init.py
+++ Nuke/init.py
@@ -6,4 +6,8 @@ nuke.pluginAddPath('./rvnuke')
 
 import nuke
 nuke.knobDefault("Root.colorManagement", "OCIO")
-nuke.knobDefault("Root.floatLut", "reference")
+#nuke.knobDefault("Root.floatLut", "reference")
+#nuke.knobDefault('Write.mov.colorspace', 'ACES 1.0 SDR-video (Rec.709 - Display)')
+nuke.knobDefault('Write.mov.mov_prores_codec_profile', '5')
+
+
--- Nuke/menu.py
+++ Nuke/menu.py
@@ -1,4 +1,7 @@
-  
+#import os
+# SET UP EXAMPLE ENVIRONMENT
+#os.environ['OCIO'] = '/Users/Derek/Documents/GitHub/OpenColorIO-configs/StdX_ACES-ANM/ANM_config.ocio'
+
   # Add EXR shuffle to read node in User tab: import shuffleLayers
 from shuffleLayers import newNode
 from shuffleLayers import mylayerPanel
@@ -8,20 +11,32 @@ toolbar = nuke.toolbar("Nodes")
 
 toolbar.addCommand( "Studio/Optical Glow", "nuke.createNode('bm_OpticalGlow2')")
 toolbar.addCommand( "Studio/Optical Lightwrap", "nuke.createNode('bm_OpticalLightwrap')")
-toolbar.addCommand( "Studio/Night Shift", "nuke.createNode('df_nightShift2')")
+#toolbar.addCommand( "Studio/Night Shift", "nuke.createNode('df_nightShift2')")
 toolbar.addCommand( "Studio/mm Color Target", "nuke.createNode('mmColorTarget')")
 #toolbar.addCommand( "Gizmos/OpticalZDefocus", "nuke.createNode('OpticalZDefocus')")
 toolbar.addCommand( "Studio/bokeh blur", "nuke.createNode('BokehBlur.gizmo')")
-toolbar.addCommand( "Studio/ACES Ref Gamut Compress", "nuke.createNode('ACES_ref_gamut_compress')")
-toolbar.addCommand( "Studio/Write Dailies", "nuke.createNode('df_WriteDailiesMov')")
 toolbar.addCommand( "Studio/FlareFactory", "nuke.createNode('FlareFactory')")
+toolbar.addCommand( "Studio/ACES Ref Gamut Compress", "nuke.createNode('ACES_ref_gamut_compress')")
+
+#toolbar.addCommand( "Studio/Burn-In", "nuke.createNode('df_burn_in')")
+#toolbar.addCommand( "Studio/Naming Variables", "nuke.createNode('df_nameVars')")
+toolbar.addCommand( "Studio/Read Footage", "nuke.createNode('df_ReadFootage')", 'shift+r' )
+toolbar.addCommand( "Studio/Write Dailies", "nuke.createNode('df_WriteDailiesMov')", 'shift+w' )
 
 
+
+
+# ROOT
+nuke.knobDefault('Root.project_directory', '[python {nuke.script_directory()}]')
+nuke.knobDefault('Root.format', 'HD_1080')
 
   # Shuffle custom defaults: 
 nuke.knobDefault("Shuffle.hide_input", "1")  
 nuke.knobDefault("Shuffle.postage_stamp", "1")
-#nuke.knobDefault("Shuffle.label", "[value in]") 
+nuke.knobDefault("Shuffle.label", "[value in1]") 
+
+# exposure custom defaults
+nuke.knobDefault("Exposure.mode", "0") 
 
   # Cryptomatte custom defaults: 
 nuke.knobDefault("Cryptomatte.hide_input", "1")  
@@ -29,7 +44,9 @@ nuke.knobDefault("Cryptomatte.hide_input", "1")
 
 # Write > Default for EXR files: DWAB
 nuke.knobDefault("Write.exr.compression","8")  
-#nuke.knobDefault('Write.beforeRender','writeDir()')
+nuke.knobDefault('Write.create_directories','1')
+ 
+
 
   # OCIO Color Space custom defaults: 
 nuke.knobDefault("OCIOColorSpace.label", "[value in_colorspace]\n[value out_colorspace]") 
@@ -48,3 +65,4 @@ def MergeNodeAfterKnobChange():
         nuke.thisNode()[ "output" ].setValue( "rgba" )
 
 nuke.addKnobChanged( MergeNodeAfterKnobChange, nodeClass="Merge2" )
+
