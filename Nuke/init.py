cfad2ab337e32c50030d646970f9cdafa90275c6
Derek Flood <23034637+sharktacos@users.noreply.github.com>
2023-03-01 09:49:31 -0800

    StdX Export Preset

--- Nuke/init.py
+++ Nuke/init.py
@@ -7,7 +7,22 @@ nuke.pluginAddPath('./rvnuke')
 import nuke
 nuke.knobDefault("Root.colorManagement", "OCIO")
 #nuke.knobDefault("Root.floatLut", "reference")
-#nuke.knobDefault('Write.mov.colorspace', 'ACES 1.0 SDR-video (Rec.709 - Display)')
+
+# default color spaces for Writes for img and mov
+nuke.knobDefault('Write.png.colorspace', 'ACES 1.0 SDR-video (Gamma 2.2 - Display)')
+nuke.knobDefault('Write.jpeg.colorspace', 'ACES 1.0 SDR-video (Gamma 2.2 - Display)')
+nuke.knobDefault('Write.mov.colorspace', 'ACES 1.0 SDR-video (Rec.709 - Display)')
+nuke.knobDefault('Write.mxf.colorspace', 'ACES 1.0 SDR-video (Rec.709 - Display)')
+
+# default mov to h.254 
+#nuke.knobDefault('Write.mov.mov64_codec', '4')
+
+# set MXF and MOV (Prores) to video range and proxy
 nuke.knobDefault('Write.mov.mov_prores_codec_profile', '5')
+nuke.knobDefault('Write.mxf.mxf_codec_profile_knob', '4')
+nuke.knobDefault('Write.mov.dataRange', '1')
+nuke.knobDefault('Write.mxf.dataRange', '1')
+
+
 
 
