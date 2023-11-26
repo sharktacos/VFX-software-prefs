440681de877fdaa63fdd0546830cde6293adfe7d
Derek Flood <23034637+sharktacos@users.noreply.github.com>
2023-11-26 12:04:14 -0800

    Update df_USD_mtlExport.py

--- Maya/scripts/df_USD_mtlExport.py
+++ Maya/scripts/df_USD_mtlExport.py
@@ -1,7 +1,7 @@
 #--------------------------------------------------------------------------------
 '''
  Export Material to USD - materials and bindings sans geometry
- v4 - add ".mtl" tag to file name
+ v5 - add "_mtl" tag to file name (fixes error with .mtl)
 
  call with:
 import df_USD_mtlExport
@@ -129,7 +129,7 @@ def main():
 
             USDlayer = filepath[0]
             stripExtension = os.path.splitext(USDlayer)[0]
-            mtl_file = stripExtension + '.mtl.usda'
+            mtl_file = stripExtension + '_mtl.usda'
             dag_root = sel[0].replace("|", "")
             root_asset = "/" + dag_root
 
